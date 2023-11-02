7e528bbb78958321076426dea648af65e3867442
Ivan Ellson <ivan.ellson.l@gmail.com>
2023-11-02 23:36:17 +0100

    Update getML.R

--- R/getML.R
+++ R/getML.R
@@ -264,7 +264,7 @@ getML <- function(expData,
     if (length(failedModels)>0) {
       message(paste0("The following models failed: ", paste0(failedModels, collapse = ", ")))
     }
-    if (!is.null(saveLogFile) & continue_on_fail==TRUE) {
+    if (file.exists(saveLogFile)) {
       message(paste0("Error and warning messages saved in ", saveLogFile))
     }
   
