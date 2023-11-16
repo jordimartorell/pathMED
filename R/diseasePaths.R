90a9f47fd84f4d3c51401eaa44741eace29a4e70
jordimartorell <33520613+jordimartorell@users.noreply.github.com>
2023-11-16 13:52:38 +0100

    Update documentation

--- R/diseasePaths.R
+++ R/diseasePaths.R
@@ -15,6 +15,8 @@
 #'
 #' @seealso \code{\link{getMscoresRef}}
 #'
+#' @import ggplot2
+#'
 #' @references Toro-Domínguez, D. et al (2022). \emph{Scoring personalized
 #' molecular portraits identify Systemic Lupus Erythematosus subtypes and
 #' predict individualized drug responses, symptomatology and
@@ -35,10 +37,10 @@ diseasePaths <- function(MRef,
   MScores <- MRef[[1]]
   genesets <- MRef[[2]]
   expr.list <- MRef[[3]]
-  
+
   if (length(MRef[[1]])==1) {min_datasets <- 1}
   min_datasets <- min(min_datasets, length(MRef))
-  
+
   HighDys.perc <- lapply(MScores, function(dat) {
     apply(dat, 1, function(x) {
       values <- as.numeric(x[!is.na(x)])
@@ -49,26 +51,26 @@ diseasePaths <- function(MRef,
     })
   })
   HighDys.perc <- do.call("cbind", HighDys.perc)
-  
+
   selected.path <- apply(HighDys.perc, 1, function(x){
     nperc <- length(x[x > perc_samples])
     ntimes <- ifelse(nperc >= min_datasets, TRUE, FALSE)
     return(ntimes)
   })
   message("Selected paths: ", sum(selected.path))
-  
+
   genesets <- genesets[selected.path]
-  
+
   # Build the expression reference
-  reference <- .MReference(expr.list=expr.list, 
+  reference <- .MReference(expr.list=expr.list,
                            mscore.list=MScores,
                            geneset.list=genesets)
-  
+
   ## plotMetrics
   if(plotMetrics){
    all<-expand.grid(1:length(MScores),1:100)
    colnames(all)<-c("min_datasets","perc_samples")
-   
+
    nPaths<-NULL
    for(i in 1:nrow(all)){
      res <- apply(HighDys.perc, 1, function(x){
@@ -80,14 +82,14 @@ diseasePaths <- function(MRef,
      nPaths<-c(nPaths,res)
    }
    all$selected_paths<-nPaths
-   
+
    agg<-aggregate(selected_paths~perc_samples,all,sum)
    agg<-agg[agg$selected_paths>=5,] ## Minimal significant pathways across
    all<-all[all$perc_samples %in% unique(agg$perc_samples),]
    all$min_datasets<-paste("min_datasets",all$min_datasets)
 
     if(nrow(all)>0){
-      P1<-ggplot2::ggplot(all,aes(x=perc_samples,y=selected_paths,group=min_datasets,color=min_datasets))+
+      P1<-ggplot(all,aes(x=perc_samples,y=selected_paths,group=min_datasets,color=min_datasets))+
        geom_line() + theme_bw()+
        geom_vline(xintercept = perc_samples, linetype = "dashed", color = "grey")+
        scale_x_continuous(breaks = round(seq(min(all$perc_samples), max(all$perc_samples), by = 5),1)) +
@@ -95,6 +97,6 @@ diseasePaths <- function(MRef,
       plot(P1)
     }
   }
-  
+
   return(list(genesets=genesets, reference=reference))
 }
