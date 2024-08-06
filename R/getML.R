101bd3b003d0647593cb4a899e7644ac3797e6e8
dtordom <42167723+dtordom@users.noreply.github.com>
2024-08-06 08:10:30 +0200

    Update getML.R

--- R/getML.R
+++ R/getML.R
@@ -520,8 +520,7 @@ getML <- function(expData,
   if (continue_on_fail == TRUE) {
     fit.model <- withCallingHandlers(tryCatch(pathMED:::.removeOutText(caret::train(group ~ ., data = newData, 
                                                                                     method = colnames(stats)[1], 
-                                                                                    tuneGrid = bestTune,
-                                                                                    classProbs =  TRUE)), 
+                                                                                    tuneGrid = bestTune)), 
                                               error = function(e) {
                                                 message(paste0("Error fitting the best model (", colnames(stats)[1], ") in all samples. NULL model returned. Try manually selecting a subset of samples and use the optimal parameters provided."))
                                                 NULL
